3f782a08bada96f4e2453b92a56950dc3335bfb0
Mert Buran <buranmert@gmail.com>
2015-07-20 19:14:30 +0300

    Pods are added to repo

---
+++ Pods/Target Support Files/Pods-MFVCKDemo-AFNetworking/Pods-MFVCKDemo-AFNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MFVCKDemo_AFNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_MFVCKDemo_AFNetworking
+@end
---
+++ Pods/Target Support Files/Pods-MFVCKDemo/Pods-MFVCKDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MFVCKDemo : NSObject
+@end
+@implementation PodsDummy_Pods_MFVCKDemo
+@end
